37631ad7cbd8ef543b573adfeb505c99d9b2fa59
Bagrat <bagrat.arutyunov.888@gmail.com>
2022-05-09 22:10:04 +0300

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from replit import clear
 import art
 
+
+def get_max_bid(dictionary: dict):
+    max_bid = -1
+    max_key = ""
+    for key in dictionary:
+        if dictionary[key] > max_bid:
+            max_bid = dictionary[key]
+            max_key = key
+    print(f"The winner is {max_key} with a bid of ${dictionary[max_key]}")
+
+
 print(art.logo)
 print("Welcome to the secret auction program.")
 
@@ -12,13 +23,7 @@ while run_auction:
     should_run = input("Are there any other bidders? Type 'yes' or 'no'.")
     if should_run == "yes":
         run_auction = True
+        clear()
     else:
         run_auction = False
-    clear()
-max_bid = -1
-max_key = ""
-for key in auction_dict:
-    if auction_dict[key] > max_bid:
-        max_bid = auction_dict[key]
-        max_key = key
-print(f"The winner is {max_key} with a bid of ${auction_dict[max_key]}")
+        get_max_bid(auction_dict)
